dd5024c27911823a839a39b433c32cd1621ad0f4
Miriam <mbb123@alu.ua.es>
2026-02-20 16:55:58 +0100

    Añadida documentacion

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace hada_p1
 {
+    /// <summary>
+    /// Clase que contiene métodos estáticos para realizar conversiones de tiempo.
+    /// </summary>
     internal class HadaP1
     {
+        /// <summary>
+        /// Convierte una cantidad de segundos a su equivalente en minutos.
+        /// </summary>
+        /// <param name="s">La cantidad de segundos a convertir (double).</param>
+        /// <returns>La cantidad de minutos resultante (double). Si la entrada es 0, devuelve 0.</returns>
         public static double Seconds2Minutes(double s)
         {
             if (s == 0)
@@ -17,11 +25,14 @@ namespace hada_p1
             return s / 60;
         }
 
-
+        /// <summary>
+        /// Convierte una cantidad de minutos a su equivalente en segundos.
+        /// </summary>
+        /// <param name="m">La cantidad de minutos a convertir (double).</param>
+        /// <returns>La cantidad de segundos resultante (double).</returns>
         public static double Minutes2Seconds(double m)
         {
             return m * 60;
         }
-
     }
 }
